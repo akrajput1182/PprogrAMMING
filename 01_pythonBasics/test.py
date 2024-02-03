8348e349aab84db5c0fdebf1186f88badba4252d
Anil Kumar <118682811+akrajput1182@users.noreply.github.com>
2024-02-03 05:37:38 +0000

    Python_Data_Types

---
+++ 01_pythonBasics/test.py
@@ -0,0 +1,51 @@
+intData = 5
+
+float_Data = 2.5
+
+String_Data = "Hello World"
+
+complex_Data = 10j
+
+list_Data = ["Anil", "Ali", "Abbas","Devil"]
+
+tuple_Data = ("yashika", "vishakha", "Anushi")
+
+dict_Data = {"Name": "AkRajput", "Age" : "20", "City":"Hell"}
+
+set_Data = {"AkRaj", "Rajput", "Mr.AkRajput"}
+
+bool_data = True
+
+print("Integer value",intData)
+print(type(intData))
+
+print(" ")
+
+print("Float value", float_Data)
+print(type(float_Data))
+
+print(" ")
+
+print("String Value: ", String_Data)
+print(type(String_Data))
+print(" ")
+
+print("Complex data: ", complex_Data)
+print(type(complex_Data))
+print(" ")
+print("list Data: ", list_Data)
+print(type(list_Data))
+print(" ")
+print("Tuple data", tuple_Data)
+print(type(tuple_Data))
+print(" ")
+print("Dictniry Data: ", dict_Data)
+print(type(dict_Data))
+print(" ")
+print("Set Data: ",set_Data)
+print(type(set_Data))
+print(" ")
+print("Boolean Data: ", bool_data)
+print(type(bool_data))
+
+
